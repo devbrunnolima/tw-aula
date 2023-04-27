bf92fcf2b5fad63b8b96df9593b761a0042ef925
devbrunnolima <devbrunnolima@gmail.com>
2023-04-27 00:38:08 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TweetComponent,
+    HomeComponent,
+    NewPostComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
